a84c2f0df064704a99089949be2b5b79ad66f13f
ljr1981 <ljr1981@msn.com>
2022-09-12 07:21:04 -0500

    deps

--- lib/DB.ex
+++
@@ -1,7 +0,0 @@
-defmodule Extut.DB do
-
-  def my_login do
-    {:ok, _pid} = MyXQL.start_link(username: "root")
-  end
-
-end
--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule Extut.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
       deps: deps()
     ]
   end
@@ -21,9 +22,27 @@ defmodule Extut.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:myxql, "~> 0.6.0"}
+      {:myxql, "~> 0.6.0"},
+      {:jason, "~> 1.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
+
+  # Aliases are shortcuts or tasks specific to the current project.
+  # For example, to install project dependencies and perform other setup tasks, run:
+  #
+  #     $ mix setup
+  #
+  # See the documentation for `Mix` for more info on aliases.
+  defp aliases do
+    [
+      setup: ["deps.get", "ecto.setup"],
+      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.reset": ["ecto.drop", "ecto.setup"],
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
+      "assets.deploy": ["esbuild default --minify", "phx.digest"]
+    ]
+  end
+
 end
